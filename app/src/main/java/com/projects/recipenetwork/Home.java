5ea994b21567e0b4373b162c60607e9d4579523e
BillChabzz <chaby9.8@gmail.com>
2015-11-17 12:16:07 +0300

    added xml files and gulotine files

--- app/src/main/java/com/projects/recipenetwork/Home.java
+++ app/src/main/java/com/projects/recipenetwork/Home.java
@@ -14,18 +14,14 @@ public class Home extends AppCompatActivity {
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_home);
+        setContentView(R.layout.content_home);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
 
-        FloatingActionButton fab = (FloatingActionButton) findViewById(R.id.fab);
-        fab.setOnClickListener(new View.OnClickListener() {
-            @Override
-            public void onClick(View view) {
-                Snackbar.make(view, "Replace with your own action", Snackbar.LENGTH_LONG)
-                        .setAction("Action", null).show();
-            }
-        });
+
+//        new GuillotineAnimation.GuillotineBuilder(guillotineMenu, guillotineMenu.findViewById(R.id.guillotine_hamburger), contentHamburger)
+//                .setActionBarViewForAnimation(toolbar)
+//                .build();
     }
 
     @Override
