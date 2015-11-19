6e2e1eb555b6a7879d21855aa8fec517cddb28b3
Wanjugu Irene <ireneemaina@gmail.com>
2015-11-19 11:03:27 +0300

    Grey Twitter Buttor error: corrected; Twitter authorization transfered from 'Login' to 'App'

--- app/src/main/java/com/projects/recipenetwork/App.java
+++ app/src/main/java/com/projects/recipenetwork/App.java
@@ -3,17 +3,42 @@ package com.projects.recipenetwork;
 import android.app.Application;
 import android.graphics.Typeface;
 
+import com.twitter.sdk.android.Twitter;
+import com.twitter.sdk.android.core.TwitterAuthConfig;
+
+import io.fabric.sdk.android.Fabric;
+
 /**
  * Created by Dmytro Denysenko on 5/6/15.
  */
 public class App extends Application {
-    private static final String CANARO_EXTRA_BOLD_PATH = "fonts/canaro_extra_bold.otf";
+
+    //Fonts
+    private static final String CANARO_EXTRA_BOLD_PATH = "assets/fonts/canaro_extra_bold.otf";
     public static Typeface canaroExtraBold;
+
+    //Twitter Keys
+    private static final String TWITTER_KEY = "cEr8HYegtclQ1afSTzBo2U2HP";
+    private static final String TWITTER_SECRET ="Hvmi1qDtJx7n5j3pnVWUNgE4vhtWSVaGHM3cOeySjApQammCrS";
+
+    //CrashLytics strings
+    public final static String CRASHLYTICS_KEY_SESSION_ACTIVATED = "session_activated";
+
+
+    private static App singleton;
+
+    public static App getInstance() {
+        return singleton;
+    }
 
     @Override
     public void onCreate() {
         super.onCreate();
-        initTypeface();
+        singleton = this;
+        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
+        Fabric.with(this, new Twitter(authConfig));
+//        initTypeface();
+
     }
 
     private void initTypeface() {
--- app/src/main/java/com/projects/recipenetwork/Login.java
+++ app/src/main/java/com/projects/recipenetwork/Login.java
@@ -2,46 +2,43 @@ package com.projects.recipenetwork;
 
 import android.content.Intent;
 import android.os.Bundle;
-import android.support.design.widget.FloatingActionButton;
-import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
-import android.support.v7.widget.Toolbar;
 import android.util.Log;
-import android.view.View;
 import android.widget.Toast;
 
 import com.twitter.sdk.android.Twitter;
 import com.twitter.sdk.android.core.Callback;
 import com.twitter.sdk.android.core.Result;
-import com.twitter.sdk.android.core.TwitterAuthConfig;
+
 import com.twitter.sdk.android.core.TwitterException;
 import com.twitter.sdk.android.core.TwitterSession;
 import com.twitter.sdk.android.core.identity.TwitterLoginButton;
 
-import io.fabric.sdk.android.Fabric;
-
 public class Login extends AppCompatActivity {
 
-    private static final String TWITTER_KEY = "cEr8HYegtclQ1afSTzBo2U2HP";
-    private static final String TWITTER_SECRET = "Hvmi1qDtJx7n5j3pnVWUNgE4vhtWSVaGHM3cOeySjApQammCrS";
     private TwitterLoginButton loginButton;
-
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.content_login);
-        TwitterAuthConfig authConfig = new TwitterAuthConfig(TWITTER_KEY, TWITTER_SECRET);
-        Fabric.with(this, new Twitter(authConfig));
+        setUpViews();
+    }
 
+    private void setUpViews() {
+       setUpLoginButton();
+    }
 
+    private void setUpLoginButton(){
         loginButton = (TwitterLoginButton) findViewById(R.id.twitter_login_button);
         loginButton.setCallback(new Callback<TwitterSession>() {
             @Override
             public void success(Result<TwitterSession> result) {
                 // The TwitterSession is also available through:
                 // Twitter.getInstance().core.getSessionManager().getActiveSession()
-                TwitterSession session = result.data;
+                TwitterSession session = Twitter.getSessionManager()
+                        .getActiveSession();
+
                 // TODO: Remove toast and use the TwitterSession's userID
                 // with your app's user model
                 String msg = "@" + session.getUserName() + " logged in! (#" + session.getUserId() + ")";
